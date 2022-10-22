8f260820ac428399c6e91f3e30424ec0fdbc29b3
Hassan Mallahzadeh <hmz1363@gmail.com>
2022-10-22 14:06:40 -0700

    delete unwanted comments

--- register.py
+++ register.py
@@ -13,7 +13,6 @@ data_template = Path(
 scan = pv.read(data_scan)
 templ = pv.read(data_template)
 templ_orig = templ.copy()
-
 n = 8000#number of points to choose
 y0 = np.sum(scan.points,axis=0)/scan.points.shape[0]
 x0 = np.sum(templ.points,axis=0)/templ.points.shape[0]
@@ -48,48 +47,3 @@ mesht = pl.add_mesh(scan, show_edges=True, color='red', opacity=0.5)
 pl.camera_position = 'xy'
 # # TODO for some reason the interactive widget doesn't work in my VSCode. Ideas?
 pl.show(title='template vs scan', jupyter_backend='static')
-# # # Tried these
-# # pl.show()
-# # pl.show(title='Digital Human',jupyter_backend='ipyvtklink')
-# # pl.show(title='Digital Human',jupyter_backend='panel')
-# # #
-# # pv.global_theme.jupyter_backend = 'pythreejs'
-# # pv.global_theme.window_size = (700, 300)
-# # pv.global_theme.antialiasing = True
-# # pl.show(title='Digital Human',jupyter_backend='pythreejs')
-
-# # %% low res mesh
-# data_mesh = Path("./data/ken/ken_low_res_r.obj")
-# mesh = pv.read(data_mesh)
-
-# pl = pv.Plotter()
-# # pl.add_mesh(mesh, show_edges=True, color='white')
-# pl.add_mesh(mesh, show_edges=True, texture=tex)
-# pl.add_axes()
-# pl.add_floor('y')
-# pl.camera_position = 'xy'
-# # TODO for some reason the interactive widget doesn't work in my VSCode. Ideas?
-# pl.show(title='Digital Human', jupyter_backend='static')
-
-# # %% visualize data on mesh
-# mesh.point_data['height'] = mesh.points[:, 1]
-# # another way to plot simple scenes
-# mesh.plot(scalars='height', cpos='xy',
-#           show_edges=True, jupyter_backend='static')
-
-# # %% modify mesh data
-# mesh.points *= np.array([2, 1, 1])
-# mesh.plot(scalars='height', cpos='xy',
-#           show_edges=True, jupyter_backend='static')
-
-# # %% filters
-# clipped = mesh.clip()
-# pl2 = pv.Plotter()
-# pl2.add_mesh(mesh, show_edges=True, color='white', opacity=0.5)
-# pl2.add_mesh(clipped)
-# pl2.add_axes()
-# pl2.add_floor('y')
-# pl2.camera_position = 'xy'
-# pl2.show(title='Digital Human', jupyter_backend='static')
-
-# # %%
